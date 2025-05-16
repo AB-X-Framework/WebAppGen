759b216e46747d2a29ebb9d79915f5caab396b2f
luis <llara@appvance.com>
2025-05-15 19:14:12 -0600

    ABX WebApp Gen

--- src/main/java/org/abx/webappgen/creds/UserPageModel.java
+++ src/main/java/org/abx/webappgen/creds/UserPageModel.java
@@ -2,9 +2,9 @@ package org.abx.webappgen.creds;
 
 import org.abx.webappgen.creds.dao.PageContentRepository;
 import org.abx.webappgen.creds.dao.SectionContentRepository;
+import org.abx.webappgen.creds.model.Component;
 import org.abx.webappgen.creds.model.Page;
 import org.abx.webappgen.creds.model.PageComponent;
-import org.abx.webappgen.creds.model.Component;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +16,9 @@ public class UserPageModel {
     public static final String Name = "name";
     public static final String Title = "title";
     public static final String Components = "components";
+    public static final String Container = "container";
     @Autowired
-    public PageContentRepository  pageContentRepository;
+    public PageContentRepository pageContentRepository;
 
     @Autowired
     public SectionContentRepository sectionContentRepository;
@@ -26,32 +27,32 @@ public class UserPageModel {
     public JSONObject getPageByPageId(long id) {
         Page page = pageContentRepository.findByPageId(id);
         JSONObject jsonPage = new JSONObject();
-        if (page == null){
-            jsonPage.put(Title,"Not found");
+        if (page == null) {
+            jsonPage.put(Title, "Not found");
             return jsonPage;
         }
-        jsonPage.put(Title,page.pageTitle);
-        jsonPage.put(Name,page.pageName);
+        jsonPage.put(Title, page.pageTitle);
+        jsonPage.put(Name, page.pageName);
         JSONArray sections = new JSONArray();
-        if (page.header){
+        if (page.header) {
             sections.put(getComponentSpecsByComponentName("header"));
         }
-        for (PageComponent pageComponent :page.pageComponents){
+        for (PageComponent pageComponent : page.pageComponents) {
             sections.put(getComponentSpecsByComponent(pageComponent.component));
         }
-        if (page.footer){
+        if (page.footer) {
             sections.put(getComponentSpecsByComponentName("footer"));
         }
-        jsonPage.put(Components,sections);
+        jsonPage.put(Components, sections);
         return jsonPage;
     }
 
-    public long elementHashCode(String element){
+    public long elementHashCode(String element) {
         return element.hashCode();
     }
 
     @Transactional
-    public long createPageWithPageName(String pageName, String pageTitle,boolean header,boolean footer) {
+    public long createPageWithPageName(String pageName, String pageTitle, boolean header, boolean footer) {
         Page page = new Page();
         page.pageName = pageName;
         page.pageId = elementHashCode(pageName);
@@ -64,7 +65,7 @@ public class UserPageModel {
 
 
     @Transactional
-    public long createComponentByComponentName(String componentName, boolean container){
+    public long createComponentByComponentName(String componentName, boolean container) {
         Component component = new Component();
         component.componentId = elementHashCode(componentName);
         component.componentName = componentName;
@@ -74,13 +75,14 @@ public class UserPageModel {
     }
 
 
-    private JSONObject getComponentSpecsByComponentName(String sectionName){
+    private JSONObject getComponentSpecsByComponentName(String sectionName) {
         return getComponentSpecsByComponent(sectionContentRepository.findBycomponentId(elementHashCode(sectionName)));
     }
 
-   private JSONObject getComponentSpecsByComponent(Component component){
+    private JSONObject getComponentSpecsByComponent(Component component) {
         JSONObject jsonSection = new JSONObject();
         jsonSection.put(Name, component.componentName);
+        jsonSection.put(Container, component.container);
         return jsonSection;
     }
 }
