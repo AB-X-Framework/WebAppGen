b23cb85e109776699a376af51ee5e78e1d79e04a
Luis Carlos Lara Lopez <llara@appvance.ai>
2025-05-18 22:12:50 -0600

    ABC WebApp Gen

--- src/main/java/org/abx/webappgen/controller/MethodController.java
+++ src/main/java/org/abx/webappgen/controller/MethodController.java
@@ -1,5 +1,8 @@
 package org.abx.webappgen.controller;
 
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
 import org.abx.webappgen.persistence.MethodModel;
 import org.abx.webappgen.persistence.PageModel;
 import org.json.JSONObject;
@@ -11,9 +14,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Set;
 
 @RestController
@@ -28,7 +36,7 @@ public class MethodController extends RoleController {
 
     @GetMapping(value = "/process/{methodName}")
     @PreAuthorize("permitAll()")
-    public ResponseEntity<byte[]> page(@PathVariable String methodName) {
+    public ResponseEntity<byte[]> page(@PathVariable String methodName, @RequestParam String args) {
         JSONObject methodSpecs = methodModel.getMethodSpec(methodName);
         if (methodSpecs == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Method " + methodName + " not found");
@@ -44,9 +52,40 @@ public class MethodController extends RoleController {
         headers.setContentDispositionFormData("attachment",
                 methodSpecs.getString("outputName"));
 
-        String r = new JSONObject(roles).toString();
-        headers.setContentLength(r.length());
-        return new ResponseEntity<>(r.getBytes(), headers, HttpStatus.OK);
+        try {
+            Object obj = processMethod(methodSpecs, args);
+            byte[] data = serialize(type, obj);
+            headers.setContentLength(data.length);
+            return new ResponseEntity<>(data, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    private byte[] serialize(String type, Object obj) throws Exception {
+        switch (type) {
+            case "json":
+            case "string":
+                return obj.toString().getBytes();
+            case "png":
+            case "jpg":
+                return toBytes((BufferedImage) obj,type);
+            default:
+                return (byte[]) obj;
+        }
+    }
+
+    public static byte[] toBytes(BufferedImage image, String format)
+            throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        boolean result = ImageIO.write(image, format, baos);
+        if (!result) {
+            throw new IOException("Unsupported image format: " + format);
+        }
+        baos.flush();
+        byte[] imageBytes = baos.toByteArray();
+        baos.close();
+        return imageBytes;
     }
 
     private MediaType contentType(String type) {
@@ -64,5 +103,22 @@ public class MethodController extends RoleController {
             default:
                 return MediaType.APPLICATION_OCTET_STREAM;
         }
+    }
+
+    private Object processMethod(JSONObject methodSpecs, String args) throws Exception {
+        Context cx = Context.newBuilder("js").allowExperimentalOptions(true).option("js.operator-overloading", "true")
+                .allowAllAccess(true)
+                .build();
+        cx.enter();
+        Value jsBindings = cx.getBindings("js");
+        String methodName = methodSpecs.getString("name");
+        JSONObject jsonArgs = methodSpecs.getJSONObject("args");
+        for (String arg : jsonArgs.keySet()) {
+            jsBindings.putMember(arg, jsonArgs.get(arg));
+        }
+        String fullSources = "function " + methodName + "(){" +
+                methodSpecs.getString("methodJS") + "} " + methodName + "();";
+        Source source = Source.newBuilder("js", fullSources, methodName).build();
+        return cx.eval(source);
     }
 }
