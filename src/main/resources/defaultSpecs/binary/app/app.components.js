6544120c2383823de2149d5335672a927bfaa5fb
Luis Carlos Lara Lopez <llara@appvance.ai>
2025-05-25 22:21:51 -0600

    ABC WebApp Gen

--- src/main/resources/defaultSpecs/binary/app/app.components.js
+++ src/main/resources/defaultSpecs/binary/app/app.components.js
@@ -1,4 +1,14 @@
 var processAction;
-function selectPackage(componentBox, packageName){
-    console.log("packageName "+packageName);
+
+function selectPackage(componentBox, packageName) {
+    $(componentBox).empty();
+    $.get(`/page/components/packages/${packageName}/components`, (resultList) => {
+        resultList.forEach(function (item) {
+            $(componentBox).append($('<option>', {
+                value: item,
+                text: item
+            }));
+        })
+        M.FormSelect.init(componentBox);
+    });
 }
