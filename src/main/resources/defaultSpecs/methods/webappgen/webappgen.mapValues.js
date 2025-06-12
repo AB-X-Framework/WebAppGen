c9ac7a0fc83706c74302cd6fa44d3e8ab2bdd95a
luis <llara@appvance.com>
2025-06-12 15:41:04 -0600

    ABX WebApp Gen

---
+++ src/main/resources/defaultSpecs/methods/webappgen/webappgen.mapValues.js
@@ -0,0 +1,13 @@
+var removeDefaults
+
+function mapValues() {
+    return JSON.stringify({
+        "specs": {},
+        "package": "webappgen.base",
+        "name": "webappgen.base.divider",
+        "isContainer": false,
+        "js": "",
+        "id": "__preview",
+        "type": "divider"
+    });
+}
