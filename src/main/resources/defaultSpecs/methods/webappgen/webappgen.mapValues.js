857ae6326dd23f82f76607f39662385e24b5618c
luis <llara@appvance.com>
2025-06-13 13:51:14 -0600

    ABX WebApp Gen

--- src/main/resources/defaultSpecs/methods/webappgen/webappgen.mapValues.js
+++ src/main/resources/defaultSpecs/methods/webappgen/webappgen.mapValues.js
@@ -9,14 +9,16 @@ function mapValues() {
     for (let i = 0; i< len;++i){
         const elem = entries.get(i);
         const key = elem.get("key");
+        const scapedKey = JSON.stringify(elem.get("key"));
         const value = elem.get("value");
+        const id = `map_${parseInt(Math.random()*1000000) }`
         children.push({
             "specs":{"title":key,"content":value},
-            js:`$(self).on('input',()=>{workingEnv.SaveMap.markChanged();workingEnv.updatedMap["${key}"]=$(${self}).val())`,
+            js:`$(${id}).on('input',()=>{workingEnv.SaveMap.markChanged();workingEnv.updatedMap[(${scapedKey})]=$(${id}).val()})`,
             "isContainer":false,
             "type":"textarea",
             "size":"l12",
-            "id":`map_${key}`
+            "id":id
         });
     }
     return JSON.stringify({
