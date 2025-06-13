7ffc7bb4752deab2cea7da36d648ba982e5e9946
luis <llara@appvance.com>
2025-06-13 15:46:38 -0600

    ABX WebApp Gen

--- src/main/resources/defaultSpecs/binary/webappgen/webappgen.maps.js
+++ src/main/resources/defaultSpecs/binary/webappgen/webappgen.maps.js
@@ -1,13 +1,4 @@
 function handleDelete(key){
-    $(workingEnv.DeletePopup.ok).click(()=>{
-        $.get(`/resources/maps/delete/${workingEnv.mapName}/${key}`,(status)=>{
-            if (status.success){
-                PageContent.closeModal(workingEnv.DeletePopup);
-                workingEnv.renderMap();
-            }else {
-                PageContent.showModal(`Cannot delete ${key}. ${JSON.stringify(status.error)}`)
-            }
-        })
-    });
+    workingEnv.setKey(key);
     PageContent.openModal(workingEnv.DeletePopup);
 }
