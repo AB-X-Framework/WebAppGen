2ecb6ba38997425f25ae74276314d22978ccad48
luis <llara@appvance.com>
2025-06-26 08:22:36 -0600

    ABX WebApp Gen

--- src/main/resources/defaultSpecs/binary/webappgen/materialize.content.js
+++ src/main/resources/defaultSpecs/binary/webappgen/materialize.content.js
@@ -162,6 +162,9 @@ class PageContent {
                 case "menuItems":
                     PageContent.renderMenuItems(output, js, componentSpecs);
                     break;
+                case "footer":
+                    PageContent.renderFooter(output, js, componentSpecs.specs);
+                    break;
                 case "img":
                     PageContent.renderImg(output, js, componentSpecs.specs);
                     break;
@@ -613,6 +616,35 @@ class PageContent {
         output.push(`  <ul class="${specs.size}"><li><a   id="${innerSpecs.id}" >${innerSpecs.title}</a></li></ul>`);
     }
 
+    static renderFooter(output, js, specs) {
+        output.push(`
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+            `);
+    }
 
     static renderMenuItems(output, js, specs) {
         let innerSpecs = specs.specs;
