6407ecccdb4fb7045520c56973fb291242cc21b6
Luis Carlos Lara Lopez <llara@appvance.ai>
2025-06-26 21:07:53 -0600

    ABC WebApp Gen

--- src/main/java/org/abx/webappgen/controller/PageController.java
+++ src/main/java/org/abx/webappgen/controller/PageController.java
@@ -1,5 +1,6 @@
 package org.abx.webappgen.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.abx.webappgen.persistence.dao.BinaryResourceRepository;
 import org.abx.webappgen.persistence.dao.MapEntryRepository;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.view.RedirectView;
 import org.stringtemplate.v4.ST;
 
@@ -38,9 +40,19 @@ public class PageController extends RoleController {
         pageTemplates = new HashMap<>();
     }
 
-    @GetMapping(value = "/{pagename}", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "/**", produces = MediaType.TEXT_HTML_VALUE)
     @PreAuthorize("permitAll()")
-    public Object page(HttpSession session, @PathVariable String pagename) {
+    public Object page(HttpSession session, HttpServletRequest request) {
+        String pagename = (String) request.getAttribute(
+                HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE
+        );
+        pagename = pagename.replaceFirst("/page/", "");
+        if (pagename.contains("/")) {
+            pagename = pagename.substring(0, pagename.indexOf("/"));
+        }
+        if (pagename.contains("?")) {
+            pagename = pagename.substring(0, pagename.indexOf("?"));
+        }
         if (!pageModel.validPage(getRoles(), elementHashCode(pagename))) {
             String home = pageModel.getHome();
             return new RedirectView("/page/" + home);
