2c4305a3a582e9646ffd4a65e75d24a6b8fd3c10
Luis Carlos Lara Lopez <llara@appvance.ai>
2025-07-02 09:03:27 -0600

    ABC WebApp Gen

--- src/main/java/org/abx/webappgen/controller/MethodController.java
+++ src/main/java/org/abx/webappgen/controller/MethodController.java
@@ -1,28 +1,12 @@
 package org.abx.webappgen.controller;
 
-import org.abx.webappgen.persistence.MethodModel;
-import org.abx.webappgen.persistence.PageModel;
-import org.abx.webappgen.persistence.UserModel;
 import org.abx.webappgen.utils.MethodExecuter;
-import org.abx.webappgen.utils.SpecsExporter;
-import org.abx.webappgen.utils.SpecsImporter;
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Source;
-import org.graalvm.polyglot.Value;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
